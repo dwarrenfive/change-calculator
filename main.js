98c6e4dc5b2b875926541c6699fcb09feaa991a7
David <dwarrenfive@gmail.com>
2019-08-31 13:16:01 -0700

    created initial sandbox layout of app

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Write your JavaScript here
+var money = [1, 0.25, 0.1, 0.05, 0.01];
+
+function calculateChange() {
+  // add formula here
+}
+
+function handleClickEvent(e) {
+  // add click handler here
+}
