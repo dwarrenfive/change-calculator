392de8573d332e71f95c792e2a118234896427d4
David <dwarrenfive@gmail.com>
2019-09-01 12:45:17 -0700

    function calculateChange is returning correct values, working on pushing values to corresponding id elements

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-var money = [1, 0.25, 0.1, 0.05, 0.01];
-
 let storePrice = document.getElementById("amount-due").value;
 let custCash = document.getElementById("amount-received").value;
 
 function calculateChange(custCash, storePrice) {
   let newAmount = custCash % storePrice;
-  for (var i = 0; i < money.length; i++) {
-    if (newAmount <= money[0]) {
-      return;
+  let change = {};
+
+  const cashValue = [100, 25, 10, 5, 1];
+  const cashType = [
+    document.getElemementById("dollars-output").innerHTML,
+    document.getElementById("quarter-output").innerHTML,
+    document.getElementById("dimes-output").innerHTML,
+    document.getElementById("nickles-output").innerHTML,
+    document.getElementById("pennies-output").innerHTML
+  ];
+
+  let amount = 0;
+
+  for (var i = 0; i < cashValue.length; i++) {
+    amount = Math.floor(newAmount / cashValue[i]);
+    if (amount > 0) {
+      change[cashType[i]] = amount;
+      newAmount = newAmount % cashValue[i];
     }
   }
+
+  return change;
 }
 
-function handleClickEvent(e) {}
+function handleClickEvent(e) {
+  let userCash = document.getElementById("amount-received").value;
+
+  let storePrice = document.getElementById("amount-due").value;
+
+  let result = calculateChange(userCash, storePrice);
+
+  document.getElementById("calculate-change").value = result;
+}
