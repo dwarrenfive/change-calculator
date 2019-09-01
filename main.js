5854b1c8833c11fcc77af3917a3f5258c9e218ad
David <dwarrenfive@gmail.com>
2019-08-31 18:27:59 -0700

    added array, initialized variables, started change function with loop and if else statement

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var money = [1, 0.25, 0.1, 0.05, 0.01];
 
-function calculateChange() {
-  // add formula here
+let storePrice = document.getElementById("amount-due").value;
+let custCash = document.getElementById("amount-received").value;
+
+function calculateChange(custCash, storePrice) {
+  let newAmount = custCash % storePrice;
+  for (var i = 0; i < money.length; i++) {
+    if (newAmount <= money[0]) {
+      return;
+    }
+  }
 }
 
-function handleClickEvent(e) {
-  // add click handler here
-}
+function handleClickEvent(e) {}
