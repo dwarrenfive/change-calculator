35e089884ded33c037eef3638d5c44a9e07015be
David <dwarrenfive@gmail.com>
2019-09-01 18:20:39 -0700

    function calculateChange is now returning what you would need for either/or elements, still debugging

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function calculateChange(custCash, storePrice) {
   var storePrice = document.getElementById("amount-due").value; // 800
   var custCash = document.getElementById("amount-received").value; // 1000
+
   var newAmount = custCash - storePrice; // 200
 
   const cashValue = [100, 25, 10, 5, 1];
@@ -8,32 +9,31 @@ function calculateChange(custCash, storePrice) {
   let amount = 0;
 
   for (var i = 0; i < cashValue.length; i++) {
-    amount = Math.floor(newAmount / cashValue[i]); // 200
-    newAmount = newAmount % cashValue[i];
-    if (amount > 100) {
-      var dollars = amount;
+    amount = newAmount % cashValue[i];
+    if (newAmount > 100) {
+      var dollars = Math.floor(newAmount / 100);
       document.getElementById(
         "dollars-output"
       ).innerHTML = `Dollars: ${dollars}`;
     }
-    if (amount > 25) {
-      var quarters = amount;
+    if (newAmount > 25) {
+      var quarters = Math.floor(newAmount / 25);
       document.getElementById(
         "quarters-output"
       ).innerHTML = `Quarters: ${quarters}`;
     }
-    if (amount > 10) {
-      var dimes = amount;
+    if (newAmount > 10) {
+      var dimes = Math.floor(newAmount / 10);
       document.getElementById("dimes-output").innerHTML = `Dimes: ${dimes}`;
     }
-    if (amount > 5) {
-      var nickels = amount;
+    if (newAmount > 5) {
+      var nickels = Math.floor(newAmount / 5);
       document.getElementById(
         "nickels-output"
       ).innerHTML = `Nickles: ${nickels}`;
     }
-    if (amount > 1) {
-      var pennies = amount;
+    if (newAmount > 1) {
+      var pennies = Math.floor(newAmount / 1);
       document.getElementById(
         "pennies-output"
       ).innerHTML = `Pennies: ${pennies}`;
